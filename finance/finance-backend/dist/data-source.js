1ba0c482a61144266d6d2d6e6522a9806df5f6a0
francoso77 <frank77invest@gmai.com>
2025-06-24 14:42:04 -0300

    docker

--- finance/finance-backend/dist/data-source.js
+++ finance/finance-backend/dist/data-source.js
@@ -12,12 +12,16 @@ var transaction_1 = require("./entity/transaction");
 var sector_1 = require("./entity/sector");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "Frk@071569#",
+    //host: "localhost",
+    //port: 3306,
+    //username: "root",
+    //password: "Frk@071569#",
     //password: "071569", //casa
-    database: "finance",
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "Frk@071569#",
+    database: process.env.DB_DATABASE || "finance",
     synchronize: true,
     logging: false,
     entities: [
--- finance/finance-backend/src/data-source.ts
+++ finance/finance-backend/src/data-source.ts
@@ -9,13 +9,18 @@ import Transaction from './entity/transaction'
 import Sector from './entity/sector'
 
 export const AppDataSource = new DataSource({
+
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "Frk@071569#",
+  //host: "localhost",
+  //port: 3306,
+  //username: "root",
+  //password: "Frk@071569#",
   //password: "071569", //casa
-  database: "finance",
+  host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "Frk@071569#",
+  database: process.env.DB_DATABASE || "finance",
   synchronize: true,
   logging: false,
   entities: [
